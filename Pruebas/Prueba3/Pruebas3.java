e8ddef9359fc0f676e2ab810ff13a72226ce63b7
jotaeme890 <permanentjavi@gmail.com>
2022-12-24 15:02:55 +0100

    Pruebas3

--- Pruebas/Prueba3/Pruebas3.java
+++ Pruebas/Prueba3/Pruebas3.java
@@ -4,6 +4,7 @@ public class Pruebas3 {
     public static void main(String[] args) {
         Empleado empleado1 = new Empleado("Paco");
         Empleado empleado2 = new Empleado("Federico");
+        Empleado empleado3 = new Empleado("Francisco");
 
         empleado1.estableceSeccion("RRHH");
         //empleado1.estableceNombre("Pedro");
@@ -12,17 +13,26 @@ public class Pruebas3 {
         System.out.println();
 
         System.out.println(empleado2.dimeDatos());
+
+        System.out.println();
+
+        System.out.println(empleado3.dimeDatos());
     }
 }
 
 class Empleado{
     //Creamos el atributo en una constante, esto hace que no se pueda modificar dicha variable en otro punto del programa
     private final String nombre;
+    private int id; 
+    //Con static lo que hacemos es que cada atributo no sea una copia si no que solo haya uno y no pertenece al objeto si no que pertenece a la clase
+    public static int idSiguiente = 1;
     private String seccion;
 
     public Empleado(String nom){
         nombre = nom;
         seccion = "Administración";
+        id = idSiguiente;
+        idSiguiente++;
     }
 
     /* 
@@ -37,6 +47,6 @@ class Empleado{
     }
 
     public String dimeDatos(){
-        return nombre + ", tu su sección es: " + seccion;
+        return nombre + ", tu su sección es: " + seccion + ". Y tú id es: " +id;
     }
 }
