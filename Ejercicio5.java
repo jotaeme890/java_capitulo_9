e6d0c9234cc69a9a176fdcb4291e3203483f951a
jotaeme890 <permanentjavi@gmail.com>
2023-01-09 18:47:36 +0100

    Ejercicio5

---
+++ Ejercicio5.java
@@ -0,0 +1,14 @@
+public class Ejercicio5 {
+    public static void main(String[] args) {
+        Pizzas p1 = new Pizzas("margarita", "mediana");
+        Pizzas p2 = new Pizzas("funghi", "familiar");
+        p2.sirve();
+        Pizzas p3 = new Pizzas("cuatro quesos", "mediana");
+        System.out.println(p1);
+        System.out.println(p2);
+        System.out.println(p3);
+        p2.sirve();
+        System.out.println("pedidas: " + Pizzas.getTotalPedidas());
+        System.out.println("servidas: " + Pizzas.getTotalServidas());
+    }
+}
