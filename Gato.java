d059ddf4729e789ea4ae85ee8f17c33cb8bccb7d
jotaeme890 <permanentjavi@gmail.com>
2023-01-14 12:48:59 +0100

    Gato

--- Gato.java
+++ Gato.java
@@ -1,11 +1,43 @@
 public class Gato extends Mamifero{
+    private String nombre;
+    private String color;
+    private String raza;
+
     public Gato () {
         super(Sexo.MACHO);
     }
 
+    public void setColor(String color){
+        this.color = color;
+    }
+
+    public void setRaza(String raza){
+        this.raza = raza;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public String getNombre(){
+        return this.nombre;
+    }
+
+    public String getColor(){
+        return this.color;
+    }
+
+    public String getRaza(){
+        return this.raza;
+    }
+
+
+
     public Gato (Sexo s) {
         super(s);
     }
+
+
 
     public void maulla(){
         System.out.println("Miauuuu");
